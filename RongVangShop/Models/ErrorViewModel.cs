d2eab0480232a8b39d12a24d7a0036a361320fd1
rongvangweb <rongvangweb@gmail.com>
2018-01-22 01:22:06 +0700

    Add project files.

---
+++ RongVangShop/Models/ErrorViewModel.cs
@@ -0,0 +1,11 @@
+using System;
+
+namespace RongVangShop.Models
+{
+    public class ErrorViewModel
+    {
+        public string RequestId { get; set; }
+
+        public bool ShowRequestId => !string.IsNullOrEmpty(RequestId);
+    }
+}
